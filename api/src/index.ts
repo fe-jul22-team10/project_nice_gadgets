5f64f89b9bd341c4b08f80f49597720b42d76d4b
Volodymyr Chyuko <vol.chuyko@gmail.com>
2022-10-30 23:10:17 +0200

    Add and config nodemon for api

--- api/src/index.ts
+++ api/src/index.ts
@@ -6,11 +6,11 @@ const app = express();
 const port = process.env.PORT || 5000; // default port to listen
 
 // define a route handler for the default home page
-app.get( "/", ( req, res ) => {
-    res.send( "Hello world!" );
-} );
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
 
 // start the Express server
-app.listen( port, () => {
-    console.log( `server started at http://localhost:${ port }` );
-} );
+app.listen(port, () => {
+  console.log(`server started at http://localhost:${port}`);
+});
