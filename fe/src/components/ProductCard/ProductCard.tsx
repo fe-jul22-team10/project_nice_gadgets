ab24861f5f3c1b6922196a46af01b527e8e007b8
roma-shafihullin <lanister0789@gmail.com>
2022-11-01 14:59:39 +0200

    class page_card, tag body removed

--- fe/src/components/ProductCard/ProductCard.tsx
+++ fe/src/components/ProductCard/ProductCard.tsx
@@ -23,58 +23,56 @@ export const ProductCard: React.FC/* <Props> */ = (/* { card } */) => {
  */
 
   return (
-    <body className="page">
-      <div className="page__card card">
-        <div className="container">
-          <img
-            src={img}
-            alt="card__img"
-            className="card__img"
-          />
-          <h2 className="card__title">
-            Apple iPhone Xs 64GB Silver (iMT9G2FS/A)
-          </h2>
-          <div className="card__price">
-            <p className="card__price-discount">
-              $799
-            </p>
-            <s className="card__price-fullPrice">
-              $899
-            </s>
-            <div className="card__price-line"></div>
-          </div>
-          <div className="card__specification">
-            <div className="card__specification-wrapper">
-              <div className="card__specification-part">
-                Screen
-              </div>
-              <div className="card__specification-value">
-                5.8” OLED
-              </div>
+    <div className="card">
+      <div className="container">
+        <img
+          src={img}
+          alt="card__img"
+          className="card__img"
+        />
+        <h2 className="card__title">
+          Apple iPhone Xs 64GB Silver (iMT9G2FS/A)
+        </h2>
+        <div className="card__price">
+          <p className="card__price-discount">
+            $799
+          </p>
+          <s className="card__price-fullPrice">
+            $899
+          </s>
+          <div className="card__price-line"></div>
+        </div>
+        <div className="card__specification">
+          <div className="card__specification-wrapper">
+            <div className="card__specification-part">
+              Screen
             </div>
-            <div className="card__specification-wrapper">
-              <div className="card__specification-part">
-                Capacity
-              </div>
-              <div className="card__specification-value">
-                64 GB
-              </div>
-            </div>
-            <div className="card__specification-wrapper">
-              <div className="card__specification-part">
-                RAM
-              </div>
-              <div className="card__specification-value">
-                4 GB
-              </div>
+            <div className="card__specification-value">
+              5.8” OLED
             </div>
           </div>
-          <div className="card__buttons">
-            <AddToCartButton />
-            <AddToFavoritesButton />
+          <div className="card__specification-wrapper">
+            <div className="card__specification-part">
+              Capacity
+            </div>
+            <div className="card__specification-value">
+              64 GB
+            </div>
+          </div>
+          <div className="card__specification-wrapper">
+            <div className="card__specification-part">
+              RAM
+            </div>
+            <div className="card__specification-value">
+              4 GB
+            </div>
           </div>
         </div>
+        <div className="card__buttons">
+          <AddToCartButton />
+          <AddToFavoritesButton />
+        </div>
       </div>
-    </body>
+    </div>
   );
 };
