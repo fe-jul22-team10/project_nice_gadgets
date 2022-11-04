48f126acc294cae91afc7782d41ca5630acfb35c
roma-shafihullin <lanister0789@gmail.com>
2022-11-04 14:13:42 +0200

    function queryParams implemented

--- fe/src/api/phones.ts
+++ fe/src/api/phones.ts
@@ -2,6 +2,24 @@ import axios from 'axios';
 
 axios.defaults.baseURL = 'https://project-nice-gadgets.herokuapp.com/';
 
-export function getPhones() {
-  return axios.get('/products');
+const obj = {
+  amount: 5,
+  page: 2,
+  year: 2019,
+};
+
+export function getQueryParams(data: any) {
+  const queryString = new URLSearchParams(data).toString();
+  return queryString;
 }
+
+const query = getQueryParams(obj);
+
+export function getPhones() {
+  return axios.get(`/products/?${query}`);
+}
+
+const products = async() => await getPhones();
+
+console.log(query);
+console.log(products());
