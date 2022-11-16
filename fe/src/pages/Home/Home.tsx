ac9db6909008be623ea69e39f2cc7aee8d96a346
roma-shafihullin <lanister0789@gmail.com>
2022-11-16 22:47:07 +0200

    loader on HomePage added

--- fe/src/pages/Home/Home.tsx
+++ fe/src/pages/Home/Home.tsx
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Banner } from '../../components/Banner';
-
+import { Loader } from '../../components/Loader';
 
 import './Home.scss';
 
 export const Home = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+  }, []);
+
   return (
-    <div className="homepage">
-      <div className="container">
-        <h1 className="homepage__title">Welcome to Nice Gadgets store!</h1>
-        <Banner />
-      </div>
+    <div>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div className="homepage">
+          <div className="container">
+            <h1 className="homepage__title">Welcome to Nice Gadgets store!</h1>
+            <Banner />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
